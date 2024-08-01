358cbc2f76e2d4c3433aa71e33476defc0352010
agape <arthur@cybrco.ai>
2024-08-01 00:18:24 -0400

    added parsing and html rendering for md files

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,19 @@
+import { generateSanitizedBlogHTML } from "/lib/postParser"
+import Markdown from 'markdown-parser-react'
+
+interface PageProps {
+	params: {
+		slug: string
+	}
+}
+
+const page = async ({ params }: PageProps) => {
+	
+	const htmlContent = await generateSanitizedBlogHTML(params.slug)
+
+	return (
+		<div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+	)
+}
+
+export default page
---
+++ lib/postParser.ts
@@ -0,0 +1,36 @@
+import path from "path"
+import { promises as fs } from "fs"
+import { Marked, Renderer } from "@ts-stack/markdown"
+
+export async function getRawBlogContents(slug: string): Promise<string> {
+	
+	const filePath: string = path.join(process.cwd(), "public", "posts", `${slug}.md`)
+
+	try {
+		const raw = await fs.readFile(filePath, "utf-8")
+		return raw
+	} catch (error) {
+		console.error("Error while reading raw blog content: ", error)
+		return null
+	}
+}
+
+export async function generateSanitizedBlogHTML(slug: string): Promise<string> {
+
+	const rawMarkdown: string = await getRawBlogContents(slug)		
+
+	Marked.setOptions ({
+		renderer: new Renderer,
+		gfm: true,
+		tables: true,
+		breaks: false,
+		pedantic: false,
+		sanitize: true,
+		smartLists: true,
+		smartypants: false
+	})
+
+	const sanitizedHTML: string =  Marked.parse(rawMarkdown)
+
+	return sanitizedHTML 
+}
